a963ae00e080f4eb67aee7b047567937d40b4d7b
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-06 11:58:29 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// One
+const filteredArray = (arr, elem) => {
+  let newArr = [];
+  
+for (let i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf(elem) == -1){
+newArr.push(arr[i]);
+      }      
+      }
+  // Only change code above this line
+  return newArr;
+}
+
+
+console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));

3b51b77430848d5d4b257abe11a58d07cdb2c9a8
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-06 20:26:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ newArr.push(arr[i]);
 
 
 console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));
+
+//two
+
+let myNestedArray = [
+  // Only change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  [
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep'],
+  [
+    ['concat', false, true, 'spread', 'array', 'deeper'],
+    [
+  ['mutate', 1327.98, 'splice', 'slice', 'push', 'deepest']
+    ]
+  ],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  ]
+  // Only change code above this line
+];
